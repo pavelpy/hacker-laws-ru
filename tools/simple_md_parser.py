c20f123601ccc885a0a9a57e8ff2bf28470d5eb6
Pavel <pavel.py3@gmail.com>
2019-06-25 14:29:58 +0300

    Some fixes in simple md parser.

--- tools/simple_md_parser.py
+++ tools/simple_md_parser.py
@@ -3,6 +3,10 @@
 """
 
 import re
+import os
+from collections import Counter
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials as ServiceAC
 
 # Anything that isn't a square closing bracket
 NAME_REGEX = "[^]]+"
@@ -12,7 +16,7 @@ MARKUP_REGEX = '\[({0})]\(\s*({1})\s*\)'.format(NAME_REGEX, LINK_REGEX)
 
 
 def main():
-    with open('../hacker-laws/README.md') as f:
+    with open(os.path.abspath('../../hacker-laws/README.md')) as f:
         for line in (l.strip() for l in f if l.strip()):
             line = line.lstrip('# *->')
             line_text = line
@@ -23,5 +27,30 @@ def main():
             print(line)
 
 
+def print_new_lines():
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+
+    credentials = ServiceAC.from_json_keyfile_name('gspread.json', scope)
+
+    gc = gspread.authorize(credentials)
+    # Open a worksheet from spreadsheet with one shot
+    worksheet = gc.open("hacker-laws-ru-readme.md").sheet1
+    values_list = list(worksheet.col_values(1))
+    counter = dict(Counter(values_list))
+
+    with open(os.path.abspath('../../hacker-laws/README.md')) as f:
+        for line in (l.strip() for l in f if l.strip()):
+            line = line.lstrip('# *->')
+            line_text = line
+
+            # Remove links.
+            for match in re.findall(MARKUP_REGEX, line_text):
+                line = line.replace('[{0}]({1})'.format(*match), '[{0}]'.format(*match))
+            if not counter.get(line):
+                print(line)
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    print_new_lines()
